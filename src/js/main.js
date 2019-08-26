ebd73ccef2f65d1d34ca97bded1828342baff277
pavlovalex <cccrazzz@gmail.com>
2019-08-26 13:23:49 +0400

    Закончил 320 breakpoint главной.

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,13 @@
 import '../stylesheets/all.scss';
 import Promise from 'promise-polyfill';
-import 'es6-object-assign/auto'; //opject assign polyfill
+import 'es6-object-assign/auto';
+import sliders from "./sliders/sliders"; //opject assign polyfill
 
-import Swiper from 'swiper';
-import ready from './helpers/_ready';
+
+
 
 if (!window.Promise) {
     window.Promise = Promise;
 }
-ready(function () {
-    var swiper = new Swiper('.swiper-container', {
-        spaceBetween: 30,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        }
-    });
-    console.log(swiper);
-});
+
+sliders();
